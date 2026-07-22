7762c2a0b817d887ef9c324db139d8134b7fec0e
Mila <mila1575@gmail.com>
2026-07-22 11:48:17 +0200

    Wait for brand fonts before rendering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const mountApp = () => {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
+
+const prepareFonts = async () => {
+  if (!document.fonts) return
+
+  await Promise.all([
+    document.fonts.load('400 1em "Arha Magnit"'),
+    document.fonts.load('500 1em "Arha Magnit"'),
+    document.fonts.load('600 1em "Arha Magnit"'),
+    document.fonts.load('700 1em "Arha Magnit"'),
+  ])
+  await document.fonts.ready
+}
+
+prepareFonts().catch(() => {}).finally(mountApp)
